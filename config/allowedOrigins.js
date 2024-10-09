14e4b74d191070102c4f4bd4d48774e49c7ec7dc
Nandakumar T <nandakumart07@gmail.com>
2024-10-09 12:01:41 +0530

    chore: update DB connection and set new CORS URL

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins=[
     'http://localhost:3500/',
+    'http://localhost:5173',  // React app (add this)
+    'http://127.0.0.1:5173',  // React app using 127.0.0.1 (add this)
     'http://www.dandrepairshop.com',
     'http://dandrepairshop.com',
 
