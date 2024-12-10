f2fcd4c721f88b71d9e670c6db8cd470a610105a
Nandakumar T <nandakumart07@gmail.com>
2024-12-10 22:45:47 +0530

    dev origin activated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins=[
     // 'http://localhost:3500/',
-    // 'http://localhost:5173',  // React app (add this)
-    // 'http://127.0.0.1:5173',  // React app using 127.0.0.1 (add this)
+    'http://localhost:5173',  // React app (add this)
+    'http://127.0.0.1:5173',  // React app using 127.0.0.1 (add this)
     // 'http://localhost:5174',  // React app (add this)
     // 'http://127.0.0.1:5174',  // React app using 127.0.0.1 (add this)
     // 'http://www.dandrepairshop.com',
